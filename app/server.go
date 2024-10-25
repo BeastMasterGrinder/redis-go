0b47c7388fcf8358fc0637302166fd9005a7abe9
Ubuntu <ubuntu@ubuntu2004.linuxvmimages.local>
2024-10-25 14:29:02 -0400

    Serve Multiple Connections Simultaniously

--- app/server.go
+++ app/server.go
@@ -11,8 +11,6 @@ func main() {
 	// You can use print statements as follows for debugging, they'll be visible when running tests.
 	fmt.Println("Logs from your program will appear here!")
 
-	// Uncomment this block to pass the first stage
-	//
 	l, err := net.Listen("tcp", "0.0.0.0:6379")
 	if err != nil {
 		fmt.Println("Failed to bind to port 6379")
@@ -25,7 +23,6 @@ func main() {
 			fmt.Println("Error accepting connection: ", err.Error())
 			continue
 		}
-		fmt.Println("Is this working?")
 
 		go handleRequest(c)
 	}
@@ -35,21 +32,19 @@ func main() {
 func handleRequest(c net.Conn) {
 	command := make([]byte, 1024)
 
-	for {
 
-		r, err := c.Read(command)
+	r, err := c.Read(command)
 
-		if err != nil {
-			fmt.Println("Error Reading from connection")
-			os.Exit(1)
-		}
+	if err != nil {
+		fmt.Println("Error Reading from connection")
+		os.Exit(1)
+	}
 
-		fmt.Printf("Read: \n%s", string(r))
-		_, err = c.Write([]byte("+PONG\r\n"))
+	fmt.Printf("Read: \n%s", string(r))
+	_, err = c.Write([]byte("+PONG\r\n"))
 
-		if err != nil {
-			fmt.Println("Error sending response")
-			os.Exit(1)
-		}
+	if err != nil {
+		fmt.Println("Error sending response")
+		os.Exit(1)
 	}
 }
